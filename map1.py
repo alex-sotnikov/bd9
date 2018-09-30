e9acdd6eb3fe32e1aee826c571553e6c128d126b
alex-sotnikov <43446052+alex-sotnikov@users.noreply.github.com>
2018-09-30 17:57:37 +0300

    Add files via upload

---
+++ map1.py
@@ -0,0 +1,11 @@
+import sys
+
+def main():
+    for line in sys.stdin:
+		ipaddress, bannerid, country, userid, payment = line.split(",")
+		payment=float(payment)
+		sys.stdout.write("{0}\t{1}\n".format(country, payment))
+
+
+if __name__ == "__main__":
+    main()
---
+++ reduce1.py
@@ -0,0 +1,24 @@
+import sys
+
+def main():
+	last_country = None
+	count = 0
+	sum_all = 0
+	for line in sys.stdin:
+		country, payment = line.strip().split("\t")
+		if last_country is None:
+			last_country = country
+			count += 1
+			sum_all += float(payment)
+		elif country == last_country:
+			count +=1
+			sum_all += float(payment)
+		else:
+			sys.stdout.write("{0}, {1}\n".format(last_country, float(sum_all)))
+			last_country = country
+			count = 1
+			sum_all = float(payment)
+	sys.stdout.write("{0}, {1}\n".format(last_country, float(sum_all)))
+
+if __name__ == "__main__":
+	main()
